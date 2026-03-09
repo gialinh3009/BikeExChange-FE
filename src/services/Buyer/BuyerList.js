3fcd4bb16aa4c03d8c9d4e0fa4dcc82b4d068aba
Phạm Mai Tâm Phúc <phucphammaitam@gmail.com>
2026-03-10 01:17:49 +0700

    update  wishlist call api

--- src/services/Buyer/BuyerList.js
+++ src/services/Buyer/BuyerList.js
@@ -1,4 +1,4 @@
-import { BASE_URL } from "../config/apiConfig";
+import { BASE_URL } from "../../config/apiConfig";
 
 export async function getBuyerListAPI({
                                           keyword = "",
@@ -25,17 +25,19 @@ export async function getBuyerListAPI({
 
     if (keyword) params.append("keyword", keyword);
     if (category_id !== undefined && category_id !== null)
-        params.append("category_id", category_id);
+        params.append("category_id", String(category_id));
     if (status) params.append("status", status);
     if (price_min !== undefined && price_min !== null)
-        params.append("price_min", price_min);
+        params.append("price_min", String(price_min));
     if (price_max !== undefined && price_max !== null)
-        params.append("price_max", price_max);
+        params.append("price_max", String(price_max));
     if (min_year !== undefined && min_year !== null)
-        params.append("min_year", min_year);
+        params.append("min_year", String(min_year));
     if (frame_size) params.append("frame_size", frame_size);
-    params.append("page", page);
-    params.append("size", size);
+
+    // page và size phải là string khi append vào URLSearchParams
+    params.append("page", String(page));
+    params.append("size", String(size));
 
     const res = await fetch(`${BASE_URL}/bikes?${params.toString()}`, {
         method: "GET",
